d08b5066ebaf6c51440cc774fde2ea696b76ba1d
zeeshan <zeeshanmohd339@gmail.com>
2025-09-07 15:14:14 +0530

    first commit

--- src/lib/tmdb.js
+++ src/lib/tmdb.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const tmdb = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_TMDB_API_URL,
+  baseURL: process.env.TMDB_API_URL,
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_TOKEN}`,
+    Authorization: `Bearer ${process.env.TMDB_TOKEN}`,
   },
 });
 
